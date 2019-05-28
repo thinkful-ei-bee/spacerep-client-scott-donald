29678b3fb295c99805382ff63e9a836b091d9a17
Scott Williams <scottcolemanwilliams@gmail.com>
2019-05-28 17:25:00 -0500

    added language api service

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import LanguageApiService from '../../services/language-api-service';
+
 
 class DashboardRoute extends Component {
+
+  state = {
+    language: {},
+    words: [],
+  }
+
+  componentDidMount() {
+    LanguageApiService.getLanguage()
+      .then(lang => 
+        this.setState({
+          language: lang.language,
+          words: lang.words
+        })
+      )
+      .then(() => console.log('state:', this.state))
+  }
+  
   render() {
     return (
       <section>
---
+++ src/services/language-api-service.js
@@ -0,0 +1,24 @@
+import config from '../config'
+import TokenService from './token-service'
+
+const LanguageApiService = {
+
+  getLanguage() {
+    return fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res => 
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+
+  
+  
+      
+}
+
+export default LanguageApiService;
