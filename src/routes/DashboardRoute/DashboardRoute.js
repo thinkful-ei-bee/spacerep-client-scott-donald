d92b3b5792a4b7020e6ecc054ff91b928e7d9ec1
Scott Williams <scottcolemanwilliams@gmail.com>
2019-05-30 17:45:48 -0500

    prep for merge

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import LanguageApiService from '../../services/language-api-service';
-import Progress from '../../components/Progress/Progress';
-import { Link } from 'react-router-dom';
+// import React, { Component } from 'react';
+// import LanguageApiService from '../../services/language-api-service';
+// import Progress from '../../components/Progress/Progress';
+// import { Link } from 'react-router-dom';
 
 
-class DashboardRoute extends Component {
+// class DashboardRoute extends Component {
 
-  state = {
-    language: {},
-    words: [],
-  }
+//   state = {
+//     language: {},
+//     words: [],
+//   }
 
-  componentDidMount() {
-    LanguageApiService.getLanguage()
-      .then(lang => 
-        this.setState({
-          language: lang.language,
-          words: lang.words
-        })
-      )
-      // .then(() => console.log('state:', this.state))
-  }
+//   componentDidMount() {
+//     LanguageApiService.getLanguage()
+//       .then(lang => 
+//         this.setState({
+//           language: lang.language,
+//           words: lang.words
+//         })
+//       )
+//       // .then(() => console.log('state:', this.state))
+//   }
   
-  render() {
-    return (
-      <>
-        <Progress language={this.state.language} words={this.state.words}/>
-        <Link to='/Learn' className='start-learning-link'>Start Learning</Link>
-      </>
-    );
-  }
-}
+//   render() {
+//     return (
+//       // <>
+//       //   <Progress language={this.state.language} words={this.state.words}/>
+//       //   <Link to='/Learn' className='start-learning-link'>Start Learning</Link>
+//       // </>
+//     );
+//   }
+// }
 
-export default DashboardRoute
+// export default DashboardRoute
