8ddb2fef14f15248bb72cb7a839eacb3eeff3895
dsonn <sbdsonn@gmail.com>
2019-05-30 15:51:18 -0400

    merging with previous personal branch

---
+++ src/components/Progress/bubblechart.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class BubbleChart extends Component {
+  renderWords = words => {
+    return words.map(word => (
+      <div className="word-div" key={word.id}>
+        <span>{word.original}</span>
+        <span>
+          {word.correct_count}/{word.correct_count + word.incorrect_count || 0}
+        </span>
+      </div>
+    ));
+  };
+
+  render() {
+    const { language, words } = this.props;
+
+    return (
+      <section className="progress-section">
+        <div className="lang-prog-div">
+          <h2>{language.name}</h2>
+          <span>total score:{language.total_score}</span>
+        </div>
+
+        <div className="word-prog-div">{this.renderWords(words)}</div>
+      </section>
+    );
+  }
+}
+
+export default BubbleChart;
