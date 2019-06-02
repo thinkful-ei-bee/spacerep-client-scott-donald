9e49909452fc715db4136fd1a3f38050eb846518
dsonn <sbdsonn@gmail.com>
2019-06-02 15:50:25 -0400

    switching from config.js to env vars

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: "https://afternoon-cove-84498.herokuapp.com/api", // the local version "http://localhost:8000/api",
-  TOKEN_KEY: "90820496-6a8f-48ca-8f6b-6baedcb3d027" // local version  "anyOtherToken"
+  TOKEN_KEY: process.env.REACT_APP_API_KEY // local version  "anyOtherToken"
 };
